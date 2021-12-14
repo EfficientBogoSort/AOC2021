abc0d712c2ea16517eb6b2d33d56d8ef23acf146
Pablo Li <76201903+EfficientBogoSort@users.noreply.github.com>
2021-12-15 01:17:12 +1100

    Optimized solution to day 13. It used to check for each point in the grid, which was not fast enough for big enough inputs. Now instead, goes over each point and deterines if it will be in the grid after the fold. If it's not, then the point is discarded, otherwise the point is updated

--- Day 13.py
+++ Day 13.py
@@ -2,61 +2,55 @@ def solve():
     with open("input.txt") as f:
         max_x = -1
         max_y = -1
-        points = []
+        points = set()
         counted = -1
         # get the max x and y to get the size of the grid
         for x in f:
             if x == '\n':
                 break
             x = x.strip().split(",")
-            points.append((int(x[0]), int(x[1])))
-            max_x = max(max_x, int(x[0]))
-            max_y = max(max_y, int(x[1]))
-        grid = [['.' for _ in range(max_x + 1)] for _ in range(max_y + 1)]
-        fill_grid(grid, points)
+            points.add((int(x[0]), int(x[1])))
         # process all folds
         for x in f.readlines():
             lin = x.strip().split(" ")[2].split("=")
-            grid = fold(grid, lin[0], int(lin[1]))
+            points = fold(lin[0], int(lin[1]), points)
             # only count the number of # after the first fold
             if counted == -1:
-                counted = count_n(grid)
-        print(counted)
+                counted = len(points)
+        for point in points:
+            max_x = max(max_x, point[0])
+            max_y = max(max_y, point[1])
+        grid = [['.' for _ in range(max_x + 1)] for _ in range(max_y + 1)]
+        fill_grid(grid, points)
+        print("Number of points after first fold:", counted)
         for x in grid:
             print(x)
+            
+            
 def fill_grid(grid, points):
     # fill all the filled spots
     for point in points:
         grid[point[1]][point[0]] = '#'
 
-def fold(grid, axis, n):
-    if axis == "x":
 
-        l = len(grid[0])
-        for y in range(len(grid)):
-            for x in range(n + 1, l):
-                c = 2 * n - x
-                if grid[y][x] == '#' and c > - 1:
-                    grid[y][c] = '#'
-        ret = []
-        # I am not sure if there's a better way to do this,
-        # but the way I figured to resize the width is to manually resize it
-        for x in grid:
-            ret.append(x[0:n])
+def fold(axis, n, points):
+    ret = points.copy()
+    if axis == "x":
+        for point in points:
+            if point[0] > n:
+                c = 2 * n - point[0]
+                if c > -1:
+                    ret.add((2 * n - point[0], point[1]))
+                ret.remove(point)
         return ret
     else:
-        h = len(grid)
-        for y in range(n + 1, h):
-            for x in range(len(grid[0])):
-                c = 2 * n - y
-                if grid[y][x] == '#' and c > -1:
-                    grid[c][x] = '#'
-        return grid[0: n]
-def count_n(grid):
-    ret = 0
-    for y in grid:
-        for x in y:
-            if x == '#':
-                ret += 1
-    return ret
+        for point in points:
+            if point[1] > n:
+                c = 2 * n - point[1]
+                if c > -1:
+                    ret.add((point[0], 2 * n - point[1]))
+                ret.remove(point)
+        return ret
+    
+    
 solve()
